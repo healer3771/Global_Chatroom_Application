ff9a47ff54d540d1d2787d6391d5ccdb849c1d9f
qadeer <qadeer2669011@gmail.com>
2023-10-02 16:24:20 -0400

    HW3

---
+++ M3/NumberGuesser4.java
@@ -0,0 +1,257 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuesser4 {
+    private int maxLevel = 1;
+    private int level = 1;
+    private int strikes = 0;
+    private int maxStrikes = 20;
+    private int number = -1;
+    private boolean pickNewRandom = true;
+    private Random random = new Random();
+    private String fileName = "ng4.txt";
+    private String[] fileHeaders = { "Level", "Strikes", "Number", "MaxLevel" };// used for demo readability
+
+    private void saveState() {
+        String[] data = { level + "", strikes + "", number + "", maxLevel + "" };
+        String output = String.join(",", data);
+        // Note: we don't need a file reference as FileWriter creates the file if it
+        // doesn't exist
+        try (FileWriter fw = new FileWriter(fileName)) {
+            fw.write(String.join(",", fileHeaders));
+            fw.write("\n");// new line
+            fw.write(output);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void loadState() {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            // Not providing output here as it's expected for a fresh start
+            return;
+        }
+        try (Scanner reader = new Scanner(file)) {
+            int lineNumber = 0;
+            while (reader.hasNextLine()) {
+                String text = reader.nextLine();
+                // System.out.println("Text: " + text);
+                if (lineNumber == 1) {
+                    String[] data = text.split(",");
+                    String level = data[0];
+                    String strikes = data[1];
+                    String number = data[2];
+                    String maxLevel = data[3];
+                    int temp = strToNum(level);
+                    if (temp > -1) {
+                        this.level = temp;
+                    }
+                    temp = strToNum(strikes);
+                    if (temp > -1) {
+                        this.strikes = temp;
+                    }
+                    temp = strToNum(number);
+                    if (temp > -1) {
+                        this.number = temp;
+                        pickNewRandom = false;
+                    }
+                    temp = strToNum(maxLevel);
+                    if (temp > -1) {
+                        this.maxLevel = temp;
+                    }
+                }
+                lineNumber++;
+            }
+        } catch (FileNotFoundException e) {// specific exception
+            e.printStackTrace();
+        } catch (Exception e2) {// any other unhandled exception
+            e2.printStackTrace();
+        }
+        System.out.println("Loaded state");
+        int range = 10 + ((level - 1) * 5);
+        System.out.println("Welcome to level " + level);
+         if(level == 1){
+            System.out.println("You are in a easy mode");}
+        System.out.println(
+                "I picked a random number between 1-" + (range) + ", let's see if you can guess.");
+    }
+
+    /***
+     * Gets a random number between 1 and level.
+     * 
+     * @param level (level to use as upper bounds)
+     * @return number between bounds
+     */
+    private void generateNewNumber(int level) {
+        int range = 10 + ((level - 1) * 5);
+        System.out.println("Welcome to level " + level);
+        if(level == 1){
+            System.out.println("You are in a easy mode");
+        }
+        System.out.println(
+                "I picked a random number between 1-" + (range) + ", let's see if you can guess.");
+        number = random.nextInt(range) + 1;
+        // Update maxStrikes based on the current level
+    }
+
+    //Name: Qadeer Ahmad
+    //UCID: qa9 
+    //Date: 02/10/2023
+    // I am creating new class which takes the level and change the maxstrikes according to it and also prints the statement which tell which mode are you in.
+    //In the next class "win" I am adding the call opotion of maxstrikeschanger with level inside the parameter to update the value when user goes to next level
+
+    private void maxstrikeschanger(int level) {
+        switch (level) {
+            case 1:
+                System.out.println("You are in a easy mode");
+                maxStrikes = 20;
+                break;
+            case 2:
+                System.out.println("You are in a medium mode");
+                maxStrikes = 15;
+                break;
+            case 3:
+                System.out.println("You are in a meimum-hard mode");
+                maxStrikes = 10;
+                break;
+            case 4:
+                System.out.println("You are in a hard mode");
+                maxStrikes = 5;
+                break;
+            case 5:
+                System.out.println("You are in a super-hard mode");
+                maxStrikes = 2;
+                break;
+            // Add more cases for higher levels if needed
+            default:
+                maxStrikes = 20; // Default value
+        }
+        System.out.println("Your maxStrikes are now: " + maxStrikes);
+    }
+
+    private void win() {
+        System.out.println("That's right!");
+        level++;// level up!
+        strikes = 0;
+        maxstrikeschanger(level);
+    }
+
+    private boolean processCommands(String message) {
+        boolean processed = false;
+        if (message.equalsIgnoreCase("quit")) {
+            System.out.println("Tired of playing? No problem, see you next time.");
+            processed = true;
+        }
+        // TODO add other conditions here
+        return processed;
+    }
+
+    //Name: Qadeer Ahmad
+    //UCID: qa9 
+    //Date: 02/10/2023
+    // I adding the class maxstrikeschanger with the level inside the parameter, this class is the one we 
+    //have created above which tell the mode and changes the strikes rate with the levels. I did this because 
+    //I want to update my strike rate and mode even after losing it and going back to privous level
+
+    private void lose() {
+        System.out.println("Uh oh, looks like you need to get some more practice.");
+        System.out.println("The correct number was " + number);
+        strikes = 0;
+        level--;
+        if (level < 1) {
+            level = 1;
+        }
+        maxstrikeschanger(level);
+    }
+    
+    //Name: Qadeer Ahmad
+    //UCID: qa9 
+    //Date: 02/10/2023
+    // I am using the if statement to add the hint comment for the user which will tell if the gussed number should be higher or lower and then printing the message
+
+    private void processGuess(int guess) {
+        if (guess < 0) {
+            return;
+        }
+        System.out.println("You guessed " + guess);
+        if (guess == number) {
+            win();
+            pickNewRandom = true;
+        } else {
+            System.out.println("That's wrong");
+            // Now use the if statment to add the hint comment for the user which will tell
+            // if the guessed number should be higher or lower and then printing the
+            // message.
+
+            strikes++;
+            if (strikes >= maxStrikes) {
+                lose();
+                pickNewRandom = true;
+            } else {
+                if (guess < number) {
+                    System.out.println("Here's A Hint For YOU: Your guess should be higher number");
+                } else {
+                    System.out.println("Here's A Hint For YOU: Your guess should be Lower number");
+                }
+            }
+        }
+    }
+
+    private int strToNum(String message) {
+        int guess = -1;
+        try {
+            guess = Integer.parseInt(message.trim());
+        } catch (NumberFormatException e) {
+            System.out.println("You didn't enter a number, please try again");
+        } catch (Exception e2) {
+            System.out.println("Null message received");
+        }
+        return guess;
+    }
+
+    public void start() {
+        try (Scanner input = new Scanner(System.in);) {
+            System.out.println("Welcome to NumberGuesser4.0");
+            System.out.println("To exit, type the word 'quit'.");
+            loadState();
+            do {
+                if (pickNewRandom) {
+                    generateNewNumber(level);
+                    saveState();
+                    pickNewRandom = false;
+                }
+                System.out.println("Type a number and press enter");
+                // we'll want to use a local variable here
+                // so we can feed it into multiple functions
+                String message = input.nextLine();
+                // early termination check
+                if (processCommands(message)) {
+                    // command handled; don't proceed with game logic
+                    break;
+                }
+                // this is just to demonstrate we can return a value and pass it into another
+                // method
+                int guess = strToNum(message);
+                processGuess(guess);
+                // the following line is the same as the above two lines
+                // processGuess(getGuess(message));
+            } while (true);
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred. Goodbye.");
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Thanks for playing!");
+    }
+
+    public static void main(String[] args) {
+        NumberGuesser4 ng = new NumberGuesser4();
+        ng.start();
+    }
+}
