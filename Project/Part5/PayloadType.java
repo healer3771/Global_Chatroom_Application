bb61da8c32d2e93fdc286d48b8748b475122b0d2
qadeer <qadeer2669011@gmail.com>
2023-11-09 14:43:29 -0500

    milestone2

--- Project/Part5/PayloadType.java
+++
@@ -1,5 +0,0 @@
-package Project.Part5;
-
-public enum PayloadType {
-    CONNECT, DISCONNECT, MESSAGE
-}
---
+++ Project/Part5/common/PayloadType.java
@@ -0,0 +1,10 @@
+package Project.Part5.common;
+
+//Qadeer Ahmad
+//Ucid: qa9
+//Date: 11/13/2023
+public enum PayloadType {
+    CONNECT, DISCONNECT, MESSAGE, CLIENT_ID, RESET_USER_LIST,
+    SYNC_CLIENT, CREATE_ROOM, JOIN_ROOM, GET_ROOMS,
+    READY
+}
